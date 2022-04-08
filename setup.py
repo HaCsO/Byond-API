aec804f251ef539da277928f8270cbb8d2bc3a49
Hacso <hacso@mail.ru>
2022-04-08 23:04:44 +0300

    Fix shit

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="Byond_API",
-    version='0.1',
+    version='0.1.0',
+    url="https://github.com/HaCsO/Byond-API",
     description="Simple way to take data from BYOND",
     packages=['Byond_API'],
     author_email="hacso@mail.ru",
-    zip_file=False
+    zip_file=False,
 )
