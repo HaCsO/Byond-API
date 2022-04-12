47f3df7ddf2ed2e950b5c45614d8b046029c30cc
Hacso <hacso@mail.ru>
2022-04-12 10:26:54 +0300

    some meta

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="Byond_API",
     version='0.2.0',
@@ -8,4 +11,10 @@ setup(
     packages=['Byond_API'],
     author_email="hacso@mail.ru",
     zip_file=False,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
 )
